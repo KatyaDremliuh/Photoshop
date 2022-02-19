71e5cda84e2f5e5880ff0953b4d291b64571969b
KatyaDremliuh <nerona.91@gmail.com>
2022-02-19 18:59:12 +0300

    TrimPixel

--- Data/Pixel.cs
+++ Data/Pixel.cs
@@ -1,9 +1,59 @@
-﻿namespace MyPhotoshop
+﻿using System;
+
+namespace MyPhotoshop
 {
     public class Pixel
     {
-        public double R;
-        public double G;
-        public double B;
+        private double r;
+
+        public double R
+        {
+            get { return r; }
+            set
+            {
+                r = Check(value);
+            }
+        }
+
+        private double g;
+
+        public double G
+        {
+            get => g;
+            set => Check(value);
+        }
+
+        private double b;
+
+        public double B
+        {
+            get => b;
+            set => Check(value);
+        }
+
+        public double Check(double value)
+        {
+            if (value < 0 || value > 1)
+            {
+                throw new ArgumentException();
+            }
+
+            return value;
+        }
+
+       public static double Trim(double value)
+        {
+            if (value<0)
+            {
+                return 0;
+            }
+
+            if (value>1)
+            {
+                return 1;
+            }
+
+            return value;
+        }
     }
 }
